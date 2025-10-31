3d9218df137a7ee6ae32c638b79fd0b478d21afe
Siddharth <123646061+Siddharth0-5@users.noreply.github.com>
2025-10-31 12:01:21 +0530

    Update team member images in teams.ts

--- data/teams.ts
+++ data/teams.ts
@@ -14,7 +14,7 @@ export const teams = [
   {
     name: "Aaryan Bhatia",
     role: "Convenor",
-    image: "/image.avif",
+    image: "/Aryan.jpg",
     bio: "A problem-solver with a strong technical foundation, he brings ideas to life with precision and practical insight. His approach ensures TEDx Manipal events are executed smoothly and effectively.",
     linkedin: "https://www.linkedin.com/in/aaryan-bhatia-aa65192b4/",
     twitter: "#",
@@ -23,7 +23,7 @@ export const teams = [
   {
     name: "Krishna Goel",
     role: "Convenor",
-    image: "/image.avif",
+    image: "/Krishna.jpg",
     bio: "Passionate about connecting people and ideas, he curates engaging programs that inspire thoughtful conversations. His focus on collaboration makes TEDx Manipal a platform for diverse voices.",
     linkedin: "https://www.linkedin.com/in/krishna-goel-38a345275/",
     twitter: "#",
@@ -32,7 +32,7 @@ export const teams = [
   {
     name: "Tanmay Shah",
     role: "Convenor",
-    image: "/image.avif",
+    image: "/Tanmay.jpg",
     bio: "With a methodical and strategic mindset, he keeps complex operations organized and reliable. His commitment to structure ensures every aspect of TEDx Manipal runs efficiently.",
     linkedin: "https://www.linkedin.com/in/tanmay-shah-23752a2b1/",
     twitter: "#",
@@ -41,7 +41,7 @@ export const teams = [
   {
     name: "Sukrit Sinha",
     role: "Managing Director",
-    image: "/image.avif",
+    image: "Sukrit.jpg",
     bio: "Famous for his innovative mindset and technical precision, he brings ideas to life through hardware and engineering excellence. His problem-solving approach and collaborative spirit ensure TEDx Manipal’s vision is powered by reliability, creativity, and cutting-edge technology.",
     linkedin: "https://www.linkedin.com/in/sukritsinha249/",
     twitter: "#",
@@ -50,7 +50,7 @@ export const teams = [
   {
     name: "Kamalnayan Panda",
     role: "Managing Director",
-    image: "/image.avif",
+    image: "Kamalnatan.jpg",
     bio: "Known for his creativity and keen eye for detail, he captures moments that tell powerful stories and inspire connection. His passion for photography and strong organizational skills help bring TEDx Manipal’s vision to life through impactful visuals and seamless teamwork.",
     linkedin: "https://www.linkedin.com/in/kamalnayanpanda/",
     twitter: "#",
